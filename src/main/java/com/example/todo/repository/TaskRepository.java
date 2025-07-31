d61502a4d7cd5eb56b470342bb64257e68541cf2
Angela <vackulencko.angel@ya.ru>
2025-08-01 00:16:10 +0400

    подключение БД, создание сущности Task, репозитория, TaskDTO

---
+++ src/main/java/com/example/todo/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.example.todo.repository;
+
+import com.example.todo.entity.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+
+}
---
+++ src/main/java/com/example/todo/service/TaskService.java
@@ -0,0 +1,14 @@
+package com.example.todo.service;
+
+import com.example.todo.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+}
